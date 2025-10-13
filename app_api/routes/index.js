d87898be030589b7f0cabbb82a851ffd33aca19f
ZacharyHancock <zacharyhancock99@gmail.com>
2025-10-13 15:10:21 -0800

    Module 6 updated with proper API routes to enable getting trip list, updating trips, and adding new ones.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,11 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const tripsController = require("../controllers/trips");
 
-router.route("/trips").get(tripsController.tripsList);
+//router.route("/trips").get(tripsController.tripsList);
 
 //GET method routes tripsFindByCode
-router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+router
+    .route("/trips")
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip); // POST method adds trip
+
+
+router
+    .route("/trips/:tripCode")
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
